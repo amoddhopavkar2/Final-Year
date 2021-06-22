297f8509e86d74eb040fb618f874bc4556269e6c
Amod Dhopavkar <amoddhopavkar2@gmail.com>
2021-06-22 07:52:42 +0530

    final commit

--- CL 9/Assignment 1/Assignment 1B/Assignment 1B code & output/ConcatServerInterface.java
+++
@@ -1,5 +0,0 @@
-import java.rmi.*;															//importing package for rmiregistry
-public interface ConcatServerInterface extends Remote
-{
-	public String concat(String S1,String S2) throws RemoteException;				//declaring function in the interface
-}
